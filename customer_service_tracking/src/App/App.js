5857d585e6e102fec047115f834585ef36b3c48c
Greg Stephen <GregAStephen@gmail.com>
2020-05-14 15:44:26 -0500

    added firebase

--- customer_service_tracking/src/App/App.js
+++ customer_service_tracking/src/App/App.js
@@ -1,14 +1,71 @@
 import React from 'react';
+import {
+  BrowserRouter as Router, Route, Redirect, Switch,
+} from 'react-router-dom';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
 import './App.scss';
 
+import NavigationBar from '../Components/NavigationBar/NavigationBar';
 import LandingPage from '../Components/LandingPage/LandingPage';
+import HomePage from '../Components/HomePage/HomePage';
+
+import fbConnect from '../Helpers/Data/fbConnection';
+
+fbConnect();
+
+const PublicRoute = ({ component: Component, authorized, ...rest }) => {
+  // props contains Location, Match, and History
+  const routeChecker = (props) => (authorized === false ? <Component authorized={authorized}{...props} {...rest} /> : <Redirect to={{ pathname: '/hpme', state: { from: props.location } }} />);
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+
+const PrivateRoute = ({ component: Component, authorized, ...rest }) => {
+  // props contains Location, Match, and History
+  const routeChecker = (props) => (authorized === true ? <Component authorized={authorized} {...props} {...rest} /> : <Redirect to={{ pathname: '/landing-page', state: { from: props.location } }} />);
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+
 
 class App extends React.Component {
+  state = {
+    authorized: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        console.error('user logged in');
+      } else {
+        this.setState({ authorized: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
+  logIn = (email, password) => {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then((cred) => cred.user.getIdToken())
+      .then((token) => sessionStorage.setItem('token', token))
+      .catch((err) => this.setState({ error: err.message }));
+  };
+
   render() {
+    const { authorized } = this.state;
     return (
       <div className="App">
-        <LandingPage/>
+        <Router>
+          <NavigationBar/>
+          <Switch>
+            <PublicRoute path='/landing-page' component={LandingPage} authorized={authorized}/>
+            <PrivateRoute path='/home' component={HomePage} authorized={authorized}/>
+            <Redirect from='*' to='/landing-page' />
+          </Switch>
+        </Router>
       </div>
     );
   }
---
+++ customer_service_tracking/src/Components/HomePage/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './HomePage.scss';
+
+class HomePage extends React.Component {
+  render() {
+    return (
+      <div className="HomePage">
+        <h1>WELCOME IN BITCHES</h1>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
