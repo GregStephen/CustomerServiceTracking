5262404a745bcbddc84a5d6d429e4356871a02ca
Greg Stephen <GregAStephen@gmail.com>
2020-05-13 21:40:52 -0500

    set up a landing page and added some planning

--- customer_service_tracking/src/App/App.js
+++ customer_service_tracking/src/App/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+
 import './App.scss';
+
+import LandingPage from '../Components/LandingPage/LandingPage';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h2>INSIDE APP COMPONENT</h2>
-        <button className="btn btn-info"><i className="fas fa-rocket"></i></button>
+        <LandingPage/>
       </div>
     );
   }
