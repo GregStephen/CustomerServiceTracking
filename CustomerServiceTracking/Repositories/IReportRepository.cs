15b72f3d34af414eb660d9ad9e675c033f596c9d
Greg Stephen <GregAStephen@gmail.com>
2020-07-19 09:14:43 -0500

    Got the backend set up to return reports and add a new report

---
+++ CustomerServiceTracking/Repositories/IReportRepository.cs
@@ -0,0 +1,14 @@
+﻿using CustomerServiceTracking.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerServiceTracking.Repositories
+{
+    public interface IReportRepository
+    {
+        IEnumerable<Report> GetReportByCustomerId(Guid customerId);
+        bool AddReport(Report newReport);
+    }
+}
---
+++ CustomerServiceTracking/Repositories/ReportRepository.cs
@@ -0,0 +1,63 @@
+﻿using CustomerServiceTracking.DataModels;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerServiceTracking.Repositories
+{
+    public class ReportRepository : IReportRepository
+    {
+        string _connectionString;
+
+        public ReportRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetValue<string>("ConnectionString");
+        }
+
+        public IEnumerable<Report> GetReportByCustomerId(Guid customerId)
+        {
+            using (var db = new SqlConnection(_connectionString))
+            {
+                var sql = @"SELECT *
+                            FROM [Report] 
+                            WHERE [CustomerId] = @customerId";
+                var parameters = new { customerId };
+                return db.Query<Report>(sql, parameters);
+            }
+        }
+
+        public bool AddReport(Report newReport)
+        {
+            using (var db = new SqlConnection(_connectionString))
+            {
+                var sql = @"INSERT INTO [Report]
+                            (
+                                [AmountRemaining],
+                                [CustomerId],
+                                [InchesAdded],
+                                [Notes],
+                                [ServiceDate],
+                                [SolutionAdded],
+                                [SystemId],
+                                [TechnicianId]
+                            )
+                            VALUES
+                            (
+                                @amountRemaining,
+                                @customerId,
+                                @inchesAdded,
+                                @notes,
+                                @serviceDate,
+                                @solutionAdded,
+                                @systemId,
+                                @technicianId
+                            )";
+                return (db.Execute(sql, newReport) == 1);
+            }
+        }
+    }
+}
