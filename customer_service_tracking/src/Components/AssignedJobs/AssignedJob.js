cd560fb7f6c7955c559576000cd98ca003e12dc7
Greg Stephen <GregAStephen@gmail.com>
2020-10-09 23:10:52 -0500

    Links to individual customer page now

--- customer_service_tracking/src/Components/AssignedJobs/AssignedJob.js
+++ customer_service_tracking/src/Components/AssignedJobs/AssignedJob.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class AssignedJob extends React.Component {
   static propTypes = {
@@ -14,9 +15,10 @@ class AssignedJob extends React.Component {
 
   render() {
     const { job } = this.props;
+    const customerLink = `/customer/${job.customer.id}`;
     return (
       <tr className="AssignedJob">
-        <td>{ job.customer.firstName } { job.customer.lastName }</td>
+        <td><Link to={{ pathname: customerLink }}>{ job.customer.firstName } { job.customer.lastName }</Link></td>
         <td>{job.customer.address.addressLine1}, {job.customer.address.city}</td>
         <td>{ job.jobType.type }</td>
         <td><button className="btn btn-danger">Make a Report</button></td>
