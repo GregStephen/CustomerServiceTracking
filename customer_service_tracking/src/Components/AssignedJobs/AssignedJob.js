c0fbf2ff1633f88f7d63e4ea7f224dc6885912f4
Greg Stephen <GregAStephen@gmail.com>
2020-10-11 21:00:47 -0500

    formats address to correct link

--- customer_service_tracking/src/Components/AssignedJobs/AssignedJob.js
+++ customer_service_tracking/src/Components/AssignedJobs/AssignedJob.js
@@ -13,13 +13,25 @@ class AssignedJob extends React.Component {
   componentDidMount() {
   }
 
+  formatThisAddress = (addressObj) => {
+    let addressStringToReturn = '';
+    const addressString1 = addressObj.addressLine1?.replace(/\s/g, '+');
+    const city = addressObj.city?.replace(/\s/g, '+');
+    const state = addressObj.state?.replace(/\s/g, '+');
+    const zip = addressObj.zipCode?.replace(/\s/g, '+');
+    addressStringToReturn = `${addressString1}+${city}+${state}+${zip}`;
+    return addressStringToReturn;
+  }
+
   render() {
     const { job } = this.props;
     const customerLink = `/customer/${job.customer.id}`;
+    const formattedAddressString = this.formatThisAddress(job.customer.address);
+    const directionLink = `https://www.google.com/maps/dir/?api=1&origin=Current+Location&destination=${formattedAddressString}`;
     return (
       <tr className="AssignedJob">
         <td><Link to={{ pathname: customerLink }}>{ job.customer.firstName } { job.customer.lastName }</Link></td>
-        <td>{job.customer.address.addressLine1}, {job.customer.address.city}</td>
+        <td><a rel="noopener noreferrer" target="_blank" href={directionLink}>{job.customer.address.addressLine1}, {job.customer.address.city}</a></td>
         <td>{ job.jobType.type }</td>
         <td><button className="btn btn-danger">Make a Report</button></td>
       </tr>
